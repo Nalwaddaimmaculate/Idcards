d399342befa4b205eadae8a6e936eb97f9b7c393
Nalwaddaimmaculate <117144141+Nalwaddaimmaculate@users.noreply.github.com>
2023-07-13 11:14:44 -0700

    Add files via upload

---
+++ src/Idcard.js
@@ -0,0 +1,18 @@
+const Idcard = () => {
+    return (
+        <div>
+            <div class="card w-25 bg-info text-white">
+                <div class="card-header bg-primary"> <h2> Jane Doe </h2></div>
+                <div class="card-body">
+                    <h4 class="card-title"> Senior Software Engineer </h4>
+                    <p class="card-text"> Phone number : 000-000-0000</p>
+                </div>
+                <div class="card-footer bg-primary">
+                   <button class="btn btn-info"> More.... </button>
+                </div>
+            </div>
+        </div>
+     );
+    }
+ 
+export default Idcard;
---
+++ src/Idcards.js
@@ -0,0 +1,19 @@
+import Idcard from "./Idcards";
+
+
+const Idcards = () => {
+    return (
+        <div>
+        <Idcard/>        
+        <Idcard/>        
+        <Idcard/>    
+        <Idcard/>    
+        <Idcard/>    
+        <Idcard/>    
+        <Idcard/>    
+        <Idcard/>
+        </div>
+     );
+}
+ 
+export default Idcards;
